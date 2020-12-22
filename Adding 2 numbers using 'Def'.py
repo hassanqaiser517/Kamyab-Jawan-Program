7a61810586291e2481e0447a7dd806de554bb3dd
hassanqaiser517 <74542928+hassanqaiser517@users.noreply.github.com>
2020-12-22 17:47:50 +0500

    Add files via upload

---
+++ Adding 2 numbers using 'Def'.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#functions
+
+
+# In[25]:
+
+
+def myfunction(a,b):
+    sum = a+b
+num1=int(input("Number: "))
+num2=int(input("Number: "))
+print("Sum is: ", num1+num2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
