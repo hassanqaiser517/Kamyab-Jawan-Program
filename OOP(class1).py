e53d6ddff231fee5febcd14bf2769bcaa353e155
hassanqaiser517 <74542928+hassanqaiser517@users.noreply.github.com>
2020-12-23 17:52:00 +0500

    Add files via upload

---
+++ OOP(class1).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+class Myclass:
+    x = 5
+    y = 12
+    z = 34
+p1 = Myclass()
+print(p1.x)
+
+
+# In[3]:
+
+
+print(p1) #if we dont connect
+
+
+# In[10]:
+
+
+p2 = Myclass()
+print(p2.z)
+
+
+# In[ ]:
+
+
+
+
