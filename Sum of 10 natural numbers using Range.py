610954578567696cf615c74b50392ea2c7775280
hassanqaiser517 <74542928+hassanqaiser517@users.noreply.github.com>
2020-12-23 17:18:56 +0500

    Add files via upload

---
+++ Sum of 10 natural numbers using Range.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+for i in range(10): 
+    print(i, end =" ") 
+print() 
+ 
+ 
+sum = 0
+for i in range(1, 10): 
+    sum = sum + i 
+print("Sum of first 10 natural number :", sum)
+
+
+# In[ ]:
+
+
+
+
