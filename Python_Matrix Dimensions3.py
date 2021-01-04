706b034093d94057ee0d7436b919f9f18809c811
Hassan Qaiser <74542928+hassanqaiser517@users.noreply.github.com>
2021-01-04 19:22:11 +0500

    Add files via upload

---
+++ Python_Matrix Dimensions3.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[55]:
+
+
+import numpy
+import numpy as np
+
+
+# In[58]:
+
+
+f = np.array([[2,3,6],
+              [3,5,8]])
+
+#print f.shape
+print(f.shape)
+h = np.array([1,2,3,4,5,6,7,8])
+
+q = h.reshape(2,2,2)
+
+print(h)
+print(q)
+
+#print q.shape
+print(q.shape)
+
+#determining Dimension
+print(q.ndim)
+
+
+# In[60]:
+
+
+w = q.reshape(-1)
+print(h)
+print(w.shape)
+print(w.ndim)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
