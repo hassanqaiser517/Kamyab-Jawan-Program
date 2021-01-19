9b9b7859836d1b7eb43375b771c4e33e4fe6734b
Hassan Qaiser <74542928+hassanqaiser517@users.noreply.github.com>
2021-01-19 18:49:42 +0500

    Add files via upload

---
+++ Python_Matplotlib1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([2, 4, 6, 8])
+b = np.array([6, 3, 8, 1])
+
+plt.plot(a, b)
+plt.show()
+
+
+# In[5]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([2, 4, 6, 8])
+b = np.array([6, 3, 8, 1])
+
+plt.plot(a,b, marker = '.', ms=12,mec='k',mfc='r')
+plt.plot(a, b)
+plt.show()
+
+
+# In[20]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([5,3,7,4])
+b = np.array([6, 1, 8, 2])
+
+plt.plot(a, marker = '.', ms=15,mec='k',mfc='b')
+plt.plot(b)
+plt.show()
+
+
+# In[23]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([5,3,7,4])
+b = np.array([6, 1, 8, 1])
+
+plt.plot(a, marker = '.', ms=12,mec='k',mfc='r')
+plt.plot(b)
+
+c = np.arange(0,2*np.pi,0.01)
+plt.plot(c,np.sin(c))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
