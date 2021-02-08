211832baa079104c8b0528fa6280b762d0d0c5a4
Hassan Qaiser <74542928+hassanqaiser517@users.noreply.github.com>
2021-02-08 18:33:53 +0500

    Add files via upload

---
+++ C++_Arduino_traffic_signal.ino
@@ -0,0 +1,29 @@
+int light1 = 11;
+int light2 = 12;
+int light3 = 13;
+
+void setup()
+{
+  pinMode(light1, OUTPUT);
+  pinMode(light2, OUTPUT);
+  pinMode(light3, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(light1, HIGH);
+delay(3000);
+digitalWrite(light1, LOW);
+
+digitalWrite(light2, HIGH);
+delay(600);
+digitalWrite(light2, LOW);
+
+digitalWrite(light3, HIGH);
+delay(3000);
+digitalWrite(light3, LOW);
+
+digitalWrite(light2, HIGH);
+delay(600);
+digitalWrite(light2, LOW);
+}
